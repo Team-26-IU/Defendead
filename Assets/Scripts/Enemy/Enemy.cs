7c4a21b6a9a0af0621c61e281163e425bf7a21d3
Thi1ef <kve100878@gmail.com>
2024-06-28 17:13:02 +0300

    added new attacker sprite and animation

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -35,13 +35,13 @@ public class Enemy : MonoBehaviour
         MoveSpeed = moveSpeed;
         _lastPointPosition = transform.position;
         DeathCoinReward = deathCoinReward;
+        Rotate();
     }
 
     private void Update()
     {
         Move();
-        Rotate();
-        
+        Rotate();   
         if (CurrentPointPositionReached())
         {
             UpdateCurrentPointIndex();
